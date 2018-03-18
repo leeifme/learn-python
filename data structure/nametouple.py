e29dcf9fe8d37206bec416dfd4900811069cc732
李磊 <1369183590@qq.com>
2018-03-18 21:36:44 +0800

    元组元素命名

--- data structure/nametouple.py
+++ data structure/nametouple.py
@@ -1,7 +1,11 @@
 from collections import namedtuple
 
-student = namedtuple('student',['name','age','sex','email']) 
+# 为元组每个元素命名 使用标准库中的collections.namedtuple
+# 元组优势： 存储空间小，访问速度快
 
-s = student('Jim','16','male','lee@leeid.me')
+student = namedtuple('student', ['name', 'age', 'sex', 'email'])
 
+s = student('Jim', '16', 'male', 'lee@leeid.me')
+
+print(s)
 print(s.name)
